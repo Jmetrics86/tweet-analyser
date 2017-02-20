74a96a51d15ec3683843e776ced6b2c1fbd29845
jhole89 <joellutman@gmail.com>
2017-02-20 23:06:32 +0000

    added semantic orientation

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@
 
 from configparser import RawConfigParser
 
+import src.term_analysis
 from src.data_sourcing import analysis_decider, data_router
 from src.data_visualisation import bar_chart
-from src.term_analysis import top_cooccorrent_terms, term_probabilities, get_lexicon
 from src.text_wrangling import document_processing
 from src.twitter_authenticate import Authorise
 
@@ -25,11 +25,14 @@ if __name__ == '__main__':
 
     word_counters, co_occ_matrix, total_docs = document_processing(all_tweets)
 
-    top_pairs = top_cooccorrent_terms(co_occ_matrix)
+    top_pairs = src.term_analysis.top_cooccorrent_terms(co_occ_matrix)
 
     most_common_terms = word_counters['terms_counter'].most_common(20)
     bar_chart(most_common_terms)
 
-    probability_of_terms = term_probabilities(total_docs, word_counters['all_counters'], co_occ_matrix)
-    positive_vocab, negative_vocab = get_lexicon()
-    
+    probability_term_matrix, probability_of_terms, = src.term_analysis.term_probabilities(total_docs,
+                                                                                          word_counters['all_counters'],
+                                                                                          co_occ_matrix)
+    pointwise_MI = src.term_analysis.calculate_pmi(probability_of_terms, co_occ_matrix, probability_term_matrix)
+    semantic_orientation = src.term_analysis.calculate_semantic_orientation(probability_of_terms, pointwise_MI)
+    top_positive_terms, top_negative_terms = src.term_analysis.top_semantic_terms(semantic_orientation, 10)
--- src/term_analysis.py
+++ src/term_analysis.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import operator
 from collections import defaultdict
+from math import log2
+from operator import itemgetter
 
 from src.data_sourcing import load_list
 
@@ -10,10 +11,10 @@ def top_cooccorrent_terms(cooccurence_matrix):
     max_matrix = []
     # For each term, look for the most common co-occurrent terms
     for term1 in cooccurence_matrix:
-        term1_max_terms = sorted(cooccurence_matrix[term1].items(), key=operator.itemgetter(1), reverse=True)[:5]
+        term1_max_terms = sorted(cooccurence_matrix[term1].items(), key=itemgetter(1), reverse=True)[:5]
         for term2, term2_count in term1_max_terms:
             max_matrix.append(((term1, term2), term2_count))
-    terms_max = sorted(max_matrix, key=operator.itemgetter(1), reverse=True)
+    terms_max = sorted(max_matrix, key=itemgetter(1), reverse=True)
     return terms_max
 
 
@@ -26,10 +27,34 @@ def term_probabilities(total_doc_count, all_terms_counter, term_cooccurence_matr
         for term2 in term_cooccurence_matrix[term1]:
             prob_term_cooccurence_matrix[term1][term2] = term_cooccurence_matrix[term1][term2] / total_doc_count
 
-    return prob_term_cooccurence_matrix
+    return prob_term_cooccurence_matrix, probability_term
 
 
-def get_lexicon():
+def calculate_pmi(probability_term, cooccurrence_matrix, prob_term_cooccurence_matrix):
+    """Calculates the Pointwise Mutual Information of each pair of terms occurring together"""
+    pmi = defaultdict(lambda: defaultdict(int))
+    for term1 in probability_term:
+        for term2 in cooccurrence_matrix[term1]:
+            denom = probability_term[term1] * probability_term[term2]
+            pmi[term1][term2] = log2(prob_term_cooccurence_matrix[term1][term2] / denom)
+    return pmi
+
+
+def calculate_semantic_orientation(probability_term, pmi):
+    semantic_orientation = {}
     positive_lexicon = load_list('resources/positive-words.txt')
     negative_lexicon = load_list('resources/negative-words.txt')
-    return positive_lexicon, negative_lexicon
+
+    for term, n in probability_term.items():
+        positive_assoc = sum(pmi[term][word] for word in positive_lexicon)
+        negative_assoc = sum(pmi[term][word] for word in negative_lexicon)
+        semantic_orientation[term] = positive_assoc - negative_assoc
+    return semantic_orientation
+
+
+def top_semantic_terms(semantic_orientation_dict, num_terms):
+    """Returns top positive and negative terms with semantic orientation"""
+    semantic_sorted = sorted(semantic_orientation_dict.items(), key=itemgetter(1), reverse=True)
+    top_positive = semantic_sorted[:num_terms]
+    top_negative = semantic_sorted[-num_terms:]
+    return top_positive, top_negative
