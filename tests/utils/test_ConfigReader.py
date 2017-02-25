eb938082edae161ae7269997c74390c34a7a5262
jhole89 <joellutman@gmail.com>
2017-02-25 11:56:36 +0000

    Add test for ConfigReader

---
+++ tests/utils/test_ConfigReader.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from utils.ConfigReader import ConfigReader
+
+
+def test_config_reader():
+    """Test should pass config properties from the properties file"""
+    test_config = ConfigReader('resources/credentials.properties')
+    assert test_config.consumer_key
+    assert test_config.consumer_secret
+    assert test_config.access_token_key
+    assert test_config.access_token_secret
