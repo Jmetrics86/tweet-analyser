3222bfdc6016c48dd4144b4cc08d2e4fb572f974
jhole89 <joellutman@gmail.com>
2017-02-26 15:12:34 +0000

    Add comments report_manager

--- src/report/data_visualisation.py
+++ src/report/data_visualisation.py
@@ -4,13 +4,19 @@ import vincent
 
 
 def bar_chart(key_freq):
+    """Generates JSON document for bar chart visualisation using vincent"""
+
     labels, freq = zip(*key_freq)
     data = {'data': freq, 'x': labels}
+
     bar = vincent.Bar(data, iter_idx='x')
     bar.to_json('report/term_freq.json')
 
 
 def timeseries_chart(pandas_timeseries):
+    """Generates JSON document for line chart visualisation using vincent"""
+
     time_chart = vincent.Line(pandas_timeseries)
     time_chart.axis_titles(x='Time', y='Freq')
+
     time_chart.to_json('report/time_series.json')
