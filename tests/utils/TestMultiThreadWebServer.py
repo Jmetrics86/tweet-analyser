615867323f80c0c1d918af4b4073a6ee4f8b22bc
jhole89 <joellutman@gmail.com>
2017-03-03 18:34:59 +0000

    Add test class for MultiThreadWebServer

--- tests/utils/TestMultiThreadWebServer.py
+++ tests/utils/TestMultiThreadWebServer.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import requests
+
+from utils.MultiThreadWebServer import MultiThreadWebServer
+
 
 class TestMultiThreadedWebServer:
+    """Test class should start up and shutdown the webserver using multithreading"""
+
     def test_threaded_server(self):
-        assert True
+        test_server = MultiThreadWebServer('localhost', 9000)
+        test_server.start_threaded_server()
+
+        test_connection = requests.get('http://localhost:9000')
+
+        test_server.stop_threaded_server()
+
+        assert test_connection.status_code == 200
